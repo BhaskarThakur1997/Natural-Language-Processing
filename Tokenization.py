01589db2fbee7a17ff01e9812f8ae590210eaac5
Bhaskar Thakur <48211637+BhaskarThakur1997@users.noreply.github.com>
2021-07-13 13:41:34 +0530

    Update Tokenization.py

--- Tokenization.py
+++ Tokenization.py
@@ -2,7 +2,8 @@
 
 import nltk
 
-paragraph = " I looked at the stars, and considered how awful it would be for a man to turn his face up to them as he froze to death, and see no help or pity in all the glittering multitude."
+paragraph = " I looked at the stars, and considered how awful it would be for a man to turn his face up to them as he froze to death, 
+              and see no help or pity in all the glittering multitude."
 
 # Tokenizing sentences
 
